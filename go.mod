0dd2f354f88e3c457db80b6dc4218438df6455c2
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-07-22 01:36:53 +1200

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/scorpionknifes/gqlopenhab
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/vektah/gqlparser/v2 v2.0.1
+)
---
+++ server.go
@@ -0,0 +1,29 @@
+package gqlopenhab
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/scorpionknifes/gql-openhab/graph"
+	"github.com/scorpionknifes/gql-openhab/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
