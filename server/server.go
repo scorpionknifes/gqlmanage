46afff06a8ff58e33b5b38fa06da21762ed6915b
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-07-22 04:31:53 +1200

    add mongodb config

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +9,38 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/scorpionknifes/gqlopenhab/graphql"
+	"github.com/scorpionknifes/gqlopenhab/mongodb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	// Connect to MongoDB
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database("tsatech")
+
+	c := graphql.Config{Resolvers: &graphql.Resolver{
+		UserRepo:   mongodb.UserRepo{DB: db.Collection("user")},
+		DeviceRepo: mongodb.DeviceRepo{DB: db.Collection("device")},
+		RoomRepo:   mongodb.RoomRepo{DB: db.Collection("room")},
+	}}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}}))
+	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(c))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
